ab8d409fc5576fa1e9c5705ea6bbe46070b147ce
thirteen <41817587+maguirre13@users.noreply.github.com>
2018-10-17 18:34:25 -0700

    first commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,38 @@
+// Display About Section
+$("#aboutLink").on("click", function() {
+  if ($("#portfolioRow").css("display") !== "none") {
+    $("#portfolioRow").fadeOut(1000, function() {
+      $("#aboutRow").fadeIn(1000);
+    });
+  } else if ($("#contactRow").css("display") !== "none") {
+    $("#contactRow").fadeOut(1000, function() {
+      $("#aboutRow").fadeIn(1000);
+    });
+  }
+});
+
+// Display Portfolio Section
+$("#portfolioLink").on("click", function() {
+  if ($("#aboutRow").css("display") !== "none") {
+    $("#aboutRow").fadeOut(1000, function() {
+      $("#portfolioRow").fadeIn(1000);
+    });
+  } else if ($("#contactRow").css("display") !== "none") {
+    $("#contactRow").fadeOut(1000, function() {
+      $("#portfolioRow").fadeIn(1000);
+    });
+  }
+});
+
+// Display Contact Section
+$("#contactLink").on("click", function() {
+  if ($("#portfolioRow").css("display") !== "none") {
+    $("#portfolioRow").fadeOut(1000, function() {
+      $("#contactRow").fadeIn(1000);
+    });
+  } else if ($("#aboutRow").css("display") !== "none") {
+    $("#aboutRow").fadeOut(1000, function() {
+      $("#contactRow").fadeIn(1000);
+    });
+  }
+});
